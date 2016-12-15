abeb8b1410cadd5b9909e8e2fb494edd2ba32dbc
Scott Zwierzynski <scottz@us.ibm.com>
2016-12-15 11:51:21 -0500

    rmv old hfgtdk dir

--- fvt/consensus/hfgtdk/ledgerstresstest/run_speed_tests.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
- go run speedtest1Min1p1Thrd.go | tee -a "GO_TEST__speedtest1Min1p1Thrd__$(date | cut -d' ' -f2-9 | sed 's/[ :]/_/g').log"
- go run speedtest1Min4p1Thrd.go | tee -a "GO_TEST__speedtest1Min4p1Thrd__$(date | cut -d' ' -f2-9 | sed 's/[ :]/_/g').log"
- go run speedtest10Min1p1Thrd.go | tee -a "GO_TEST__speedtest10Min1p1Thrd__$(date | cut -d' ' -f2-9 | sed 's/[ :]/_/g').log"
- go run speedtest10Min4p1Thrd.go | tee -a "GO_TEST__speedtest10Min4p1Thrd__$(date | cut -d' ' -f2-9 | sed 's/[ :]/_/g').log"
-
- grep FINAL GO_TEST__speedtest1*Min*p*Thrd__*
